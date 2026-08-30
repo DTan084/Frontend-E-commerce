00d410a2ea6e29eaab6ae25a6eb0dedf8ddea89c
Dinh Tan <DTan084@users.noreply.github.com>
2026-08-30 17:27:43 +0700

    feat(routes): register forgot-password route in auth paths and routes

--- src/routes/paths.js
+++ src/routes/paths.js
@@ -7,6 +7,7 @@ export const ROUTE_PATHS = {
     ROOT: '/auth',
     LOGIN: 'login',
     REGISTER: 'register',
+    FORGOT_PASSWORD: 'forgot-password',
   },
   PUBLIC: {
     PRODUCTS: 'products',
